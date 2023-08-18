6f090baa8ff7378e3cce7281c593a46592e3eb17
Kamil <perlegus@gmail.com>
2023-08-18 16:08:39 +0200

    added position fixed to header, created list of cards with scroll and single card view

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,16 @@ import { AppLayout } from './components/AppLayout';
 import './App.css';
 import { CardFront } from './components/CardFront.tsx';
 import { CardBack } from './components/CardBack.tsx';
+import { SingleCard } from './components/SingleCard.tsx';
+import { CardsList } from './components/CardsList.tsx';
 
 function App() {
   return (
     <AppLayout>
       <AppHeader />
+      <CardsList />
       {/*<CardFront />*/}
-      <CardBack />
+      {/*<CardBack />*/}
     </AppLayout>
   );
 }
---
+++ src/components/SingleCard.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import editButton from '../assets/editButton.png';
+import styles from './SingleCard.module.css';
+
+export const SingleCard = () => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.text}>
+        <p>Przykładowy tekst testowy dla wystylowania układu tekstuaaaaaaaa</p>
+      </div>
+      <div className={styles.icon}>
+        <img src={editButton} alt="edit-button" />
+      </div>
+    </div>
+  );
+};
